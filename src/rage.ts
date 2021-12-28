b1c4641ca12ab31e2d244beb80df73fce3b7723b
xiorcal <xiorcal@protonmail.com>
2021-12-28 18:43:12 +0100

    refactor: removed music capability
    Functionnality wasn't working properly, so I decided to remove it

--- src/rage.ts
+++ src/rage.ts
@@ -9,7 +9,6 @@ import { ScoreBoard } from './scoreboard';
 import StormDB from 'stormdb';
 import { Freebies } from './freeGamez';
 import * as cron from 'node-cron';
-import ytdl from 'ytdl-core';
 
 dotenv.config();
 
@@ -117,17 +116,6 @@ function handleCommand(message: Discord.Message) {
     case 'replace_miraltar':
       handleRegisterFreebies(message);
       break;
-    case 'p':
-    case 'play':
-      handlePlay(message);
-      break;
-    case 'skip':
-      handleSkip(message);
-      break;
-    case 'stop':
-    case 'leave':
-      handleStop(message);
-      break;
     default:
       break;
   }
@@ -148,101 +136,6 @@ function handleCreate(message: Discord.Message, title: string) {
           .then(() => newMessage.pin());
       });
   }
-}
-
-async function handlePlay(message: Discord.Message) {
-  const args = message.content.split(' ');
-
-  const voiceChannel = message.member.voice.channel;
-  if (!voiceChannel)
-    return message.channel.send(
-      'You need to be in a voice channel to play music!',
-    );
-  const permissions = voiceChannel.permissionsFor(message.client.user);
-  if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
-    return message.channel.send(
-      'I need the permissions to join and speak in your voice channel!',
-    );
-  }
-
-  const songInfo = await ytdl.getInfo(args[1]);
-  const song = {
-    title: songInfo.videoDetails.title,
-    url: songInfo.videoDetails.video_url,
-  };
-  const serverQueue = queues.get(message.guild.id);
-  if (!serverQueue) {
-    const queueContruct = {
-      textChannel: message.channel,
-      voiceChannel: voiceChannel,
-      connection: null,
-      songs: [],
-      volume: 5,
-      playing: true,
-    };
-
-    queues.set(message.guild.id, queueContruct);
-
-    queueContruct.songs.push(song);
-
-    try {
-      const connection = await voiceChannel.join();
-      queueContruct.connection = connection;
-      play(message.guild, queueContruct.songs[0]);
-    } catch (err) {
-      console.log(err);
-      queues.delete(message.guild.id);
-      return message.channel.send(err);
-    }
-  } else {
-    serverQueue.songs.push(song);
-    return message.channel.send(`${song.title} has been added to the queue!`);
-  }
-}
-
-function play(guild: Discord.Guild, song: any) {
-  const serverQueue = queues.get(guild.id);
-  if (!song) {
-    serverQueue.voiceChannel.leave();
-    queues.delete(guild.id);
-    return;
-  }
-
-  const dispatcher = serverQueue.connection
-    .play(ytdl(song.url))
-    .on('finish', () => {
-      serverQueue.songs.shift();
-      play(guild, serverQueue.songs[0]);
-    })
-    .on('error', (error) => console.error(error));
-  dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-  serverQueue.textChannel.send(`Start playing: **${song.title}**`);
-}
-function handleSkip(message: Discord.Message) {
-  const serverQueue = queues.get(message.guild.id);
-
-  if (!message.member.voice.channel)
-    return message.channel.send(
-      'You have to be in a voice channel to stop the music!',
-    );
-  if (!serverQueue)
-    return message.channel.send('There is no song that I could skip!');
-  serverQueue.connection.dispatcher.end();
-}
-
-function handleStop(message: Discord.Message) {
-  const serverQueue = queues.get(message.guild.id);
-
-  if (!message.member.voice.channel)
-    return message.channel.send(
-      'You have to be in a voice channel to stop the music!',
-    );
-
-  if (!serverQueue)
-    return message.channel.send('There is no song that I could stop!');
-
-  serverQueue.songs = [];
-  serverQueue.connection.dispatcher.end();
 }
 
 function handleLogs(message: Discord.Message) {
